e348a0436042d0939b216d5ced1a6461caf7b6cc
0xCyll <57342571+0xCyll@users.noreply.github.com>
2023-07-13 19:23:43 +0100

    Update main.py
    tiny bit of a cute rework

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ import mss
 import os
 import webbrowser
 import datetime
+import json
 
 
 upload_url = "https://api.nightlight.gg/v1/upload"
-authorization_token = "YOUR TOKEN HERE" # get one from https://nightlight.gg/account/api
-
 fonte = "Helvetica"
 screenshot_folder = os.path.join(os.path.expanduser("~"), "Documents", "cylllight")
 
@@ -19,7 +18,7 @@ os.makedirs(screenshot_folder, exist_ok=True)
 window = tk.Tk()
 window.title("Cylllight")
 window.configure(bg="black")
-icon_path = "your.ico"  #put your own path for icon or smth
+icon_path = "Path2icon.ico"  # put your own path for the icon
 window.iconbitmap(icon_path)
 style = ttk.Style()
 style.theme_use("clam")
@@ -48,24 +47,31 @@ url_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 url_listbox = tk.Listbox(url_frame, height=10, yscrollcommand=url_scrollbar.set)
 url_listbox.pack(fill=tk.BOTH, expand=True)
 url_scrollbar.config(command=url_listbox.yview)
+
 def open_selected_url():
     selected_index = url_listbox.curselection()
     if selected_index:
         url = url_listbox.get(selected_index[0])
         webbrowser.open(url)
+        url_listbox.itemconfig(selected_index[0], bg="white", fg="black")  
 
 open_button = ttk.Button(window, text="Open URL", command=open_selected_url)
 open_button.grid(row=2, column=0, padx=10, pady=10, sticky=tk.W)
+
 def show_screenshot_folder():
     os.startfile(screenshot_folder)
+
 folder_button = ttk.Button(window, text="Show Folder", command=show_screenshot_folder)
 folder_button.grid(row=2, column=1, padx=10, pady=10, sticky=tk.W)
+
 def remove_selected_url():
     selected_index = url_listbox.curselection()
     if selected_index:
         url_listbox.delete(selected_index[0])
+
 remove_button = ttk.Button(window, text="Remove URL", command=remove_selected_url)
 remove_button.grid(row=2, column=2, padx=10, pady=10, sticky=tk.W)
+
 class MessageDialog(tk.Toplevel):
     def __init__(self, parent, title, message):
         super().__init__(parent)
@@ -76,34 +82,87 @@ class MessageDialog(tk.Toplevel):
         label.pack(padx=20, pady=20)
         ok_button = ttk.Button(self, text="OK", command=self.destroy)
         ok_button.pack(pady=10)
+
+def configure_api_key():
+    config_file_path = os.path.join(screenshot_folder, "config.json")
+    if os.path.exists(config_file_path):
+        with open(config_file_path, "r") as config_file:
+            config = json.load(config_file)
+            api_key = config.get("api_key")
+    else:
+        api_key = None
+
+    dialog = tk.Toplevel(window)
+    dialog.title("API Key Configuration")
+
+    def save_api_key():
+        nonlocal api_key
+        api_key = api_key_entry.get().strip()
+        with open(config_file_path, "w") as config_file:
+            json.dump({"api_key": api_key}, config_file)
+        dialog.destroy()
+
+    def show_api_key():
+        MessageDialog(dialog, "API Key", f"Your current API key is: {api_key}")
+
+    dialog_label = ttk.Label(dialog, text="Please enter your API key:")
+    dialog_label.pack(padx=20, pady=20)
+
+    api_key_entry = ttk.Entry(dialog, width=30)
+    api_key_entry.pack(padx=20)
+    if api_key:
+        api_key_entry.insert(tk.END, api_key)
+
+    save_button = ttk.Button(dialog, text="Save", command=save_api_key)
+    save_button.pack(pady=10)
+
+    show_button = ttk.Button(dialog, text="Show API Key", command=show_api_key)
+    show_button.pack(pady=10)
+
 def take_screenshot_and_upload():
     current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     filename = f"{current_time}_1.jpg"
     screenshot_file = os.path.join(screenshot_folder, filename)
     with mss.mss() as sct:
         sct.shot(output=screenshot_file)
+
+    config_file_path = os.path.join(screenshot_folder, "config.json")
+    if not os.path.exists(config_file_path):
+        configure_api_key()
+        return
+
     with open(screenshot_file, "rb") as file:
+        with open(config_file_path, "r") as config_file:
+            config = json.load(config_file)
+            api_key = config.get("api_key")
+
         response = requests.post(
             upload_url,
-            headers={"Authorization": f"Bearer {authorization_token}"},
+            headers={"Authorization": f"Bearer {api_key}"},
             files={"file": file}
         )
+
     if response.status_code == 200:
         response_label.config(text="Screenshot uploaded successfully!")
         response_data = response.json().get("data", {})
         url = response_data.get("url")
         if url:
             url_label.config(text=f"URL: {url}")
-            url_listbox.insert(tk.END, url)  
+            url_listbox.insert(0, url)  
+            url_listbox.itemconfig(0, bg="green", fg="white")  
     else:
         response_label.config(text="Upload failed!")
         error_label.config(text=f"Error: {response.json().get('error', {}).get('message')}")
+
 response_label = ttk.Label(window, text="")
 response_label.grid(row=3, column=0, padx=10, pady=10, sticky=tk.W)
+
 url_label = ttk.Label(window, text="")
 url_label.grid(row=4, column=0, padx=10, pady=10, sticky=tk.W)
+
 error_label = ttk.Label(window, text="")
 error_label.grid(row=5, column=0, padx=10, pady=10, sticky=tk.W)
+
 def on_key_press(key):
     try:
         if key.char == trigger_entry.get():
@@ -113,4 +172,8 @@ def on_key_press(key):
 
 listener = keyboard.Listener(on_press=on_key_press)
 listener.start()
+
+configure_button = ttk.Button(window, text="Configure API Key", command=configure_api_key)
+configure_button.grid(row=6, column=0, padx=10, pady=10, sticky=tk.W)
+
 window.mainloop()
